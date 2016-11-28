781a4573893d4d7e68cdb8a9d9eb551c07bbdd5b
Keith Underwood <keithunderwood510@gmail.com>
2016-11-28 14:39:50 -0800

    Adjust ajax calls to include client id, refactor some js

--- js/app.js
+++ js/app.js
@@ -1,62 +1,93 @@
-$(function(){
-  $('#search-btn').click(function(e){
+/*eslint semi: ["error", "always"]*/
+// twitch api server does not allow cross-origin requests so jsonp must be used
+// all requests must include headers to define Client_Id and Accept: (define api version)
+
+function clearView() {
+  $('#mods-list').html("");
+  $('#chatters-list').html("");
+  $('#user-details').html("");
+}
+
+function populateUserDetails(data) {
+  $('#user-details').html("");
+  $('#user-details').append("<li>Date Created: " + data.users[0].created_at + "</li>");
+  $('#user-details').append("<li><img src='" + data.users[0].logo + "'></li>");
+  $('#user-details').append("<li>Bio: " + data.users[0].bio + "</li>");
+}
+
+// populates the viewers list and attaches onClick to each user
+function populateViewers(data) {
+  if (data.data.chatter_count <= 1) {
+    alert('Did not find any data. Did you check the spelling?');
+  }
+  var viewersArray = data.data.chatters.viewers;
+  var randomViewer = viewersArray[Math.floor(Math.random() * viewersArray.length)];
+
+  data.data.chatters.moderators.forEach(function(mod) {
+    var liMod = $("<li><a>" + mod + "</a></li>");
+    $('#mods-list').append(liMod);
+    liMod.click(function() {
+      var url = `https://api.twitch.tv/kraken/users?login=${mod}&api_version=5&client_id=apc58vh6azavxes7g26dbazltd9em3a`;
+      $.ajax({
+          dataType: 'json',
+          url: url
+        })
+        .done(function(data) {
+          populateUserDetails(data);
+        });
+      $('#user h4').html(`${mod}:`);
+    });
+  });
+
+  viewersArray.forEach(function(viewer) {
+    var liViewer = $("<li><a>" + viewer + "</a></li>");
+    $('#chatters-list').append(liViewer);
+    liViewer.click(function() {
+      var url = `https://api.twitch.tv/kraken/users?login=${viewer}&api_version=5&client_id=apc58vh6azavxes7g26dbazltd9em3a`;
+      $.ajax({
+          dataType: 'json',
+          url: url
+        })
+        .done(function(data) {
+          populateUserDetails(data);
+        });
+      $('#user h4').html(`${viewer}:`);
+    });
+  });
+
+  $('#random').click(function(e) {
+    var randomViewer = viewersArray[Math.floor(Math.random() * viewersArray.length)];
+    var url = `https://api.twitch.tv/kraken/users?login=${randomViewer}&api_version=5&client_id=apc58vh6azavxes7g26dbazltd9em3a`;
+    $.ajax({
+        dataType: 'json',
+        url: url
+      })
+      .done(function(data) {
+        populateUserDetails(data);
+      });
+    $('#user h4').html(`${randomViewer}:`);
+  });
+  console.log(data);
+}
+
+$(function() {
+  $('#search-btn').click(function(e) {
     e.preventDefault();
+    clearView();
     var streamer = $('#search-box').val().toLowerCase();
-    $("#stream-title").html($('#search-box').val())
-    $.getJSON("https://tmi.twitch.tv/group/user/"+streamer+"/chatters?callback=?",function(data){
-      var viewersArray = data.data.chatters.viewers
-      var randomViewer = viewersArray[Math.floor(Math.random() * viewersArray.length)];
-      $('#mods-list').html("")
-
-      $('#chatters-list').html("")
-      $('#user-details').html("")
-      data.data.chatters.moderators.forEach(function(mod){
-        var liMod = $("<li><a>"+mod+"</a></li>");
-        $('#mods-list').append(liMod)
-        liMod.click(function() {
-          $.getJSON("https://api.twitch.tv/kraken/users/"+mod+"?callback=?",function(data){
-            $('#user-details').html("")
-            $('#user h4').html(mod+":");
-            $('#user-details').append("<li>Date Created: "+data.created_at+"</li>" )
-            $('#user-details').append("<li><img src='"+data.logo+"'></li>")
-            $('#user-details').append("<li>Bio: "+data.bio+"</li>" )
-          })
-          $('#user h4').html(mod+":");
-
-        })
-      })
-
-      viewersArray.forEach(function(viewer){
-        var liViewer = $("<li><a>"+viewer+"</a></li>");
-        $('#chatters-list').append(liViewer)
-        liViewer.click(function() {
-          $.getJSON("https://api.twitch.tv/kraken/users/"+viewer+"?callback=?",function(data){
-            console.log(data);
-            $('#user-details').html("")
-            $('#user h4').html(viewer+":");
-            $('#user-details').append("<li>Date Created: "+data.created_at+"</li>" )
-            $('#user-details').append("<li><img src='"+data.logo+"'></li>")
-            $('#user-details').append("<li>Bio: "+data.bio+"</li>" )
-          })
-
-        })
-      })
-      $('#random').click(function(e){
-        var randomViewer = viewersArray[Math.floor(Math.random() * viewersArray.length)];
-        $.getJSON("https://api.twitch.tv/kraken/users/"+randomViewer+"?callback=?",function(data){
-          console.log(data);
-          $('#user-details').html("")
-          $('#user h4').html(data.name+":");
-          $('#user-details').append("<li>Date Created: "+data.created_at+"</li>" )
-          $('#user-details').append("<li><img src='"+data.logo+"'></li>")
-          $('#user-details').append("<li>Bio: "+data.bio+"</li>" )
-        })
-      })
-
-      console.log(data);
+    $("#stream-title").html($('#search-box').val());
+      // uses chatters api (unsupported twitch) to get chatters list
+      // chatters api may break when twitch api defaults to version 5
+    var url = `https://tmi.twitch.tv/group/user/${streamer}/chatters`;
+    $.ajax({
+      dataType: 'jsonp',
+      url: url
     })
-
-
-
-  })
-})
+      .done(function(data) {
+        populateViewers(data);
+      })
+      .fail(function(data) {
+        alert('Request failed.');
+      });
+  });
+});
